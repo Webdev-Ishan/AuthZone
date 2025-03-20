087c67b319e2b385ffcb836388023c40d22048ff
Ishan Saini <ishansaini0105@gmail.com>
2025-03-20 16:41:40 +0530

    The auth controllers are created

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -36,6 +36,59 @@ export const register = async (req, res) => {
       maxAge: 1 * 24 * 60 * 60 * 1000,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     });
+
+    return res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.json({ success: false, message: "email or password is wrong" });
+  }
+
+  try {
+    const existuser = await userModel.findOne({ email });
+    if (!existuser) {
+      res.json({ success: false, message: "user is not registered" });
+    }
+
+    const check = await bcrypt.compare(password.user.password);
+
+    if (!check) {
+      res.json({ success: false, message: "user is not registered" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("token", "", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    });
+
+    return res.json({ success: true, message: "User logged out successfully" });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
