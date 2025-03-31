e3ee12e1d8dbabab507e66ad4f9915334b34fab3
Ishan Saini <ishansaini0105@gmail.com>
2025-03-31 17:08:54 +0530

    Update user.Auth.js

--- Server/middleware/user.Auth.js
+++ Server/middleware/user.Auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
   if (!req.cookies.token) {
-    return res.json({ success: false, message: "Not Authorized login again" });
+    return res.json({ success: false, message: "Not Authorized " });
   }
 
   try {
