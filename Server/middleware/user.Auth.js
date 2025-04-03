b0f338a0285b895f49fb2af281c586d0e5798974
Ishan Saini <ishansaini0105@gmail.com>
2025-04-03 17:13:27 +0530

    Update user.Auth.js

--- Server/middleware/user.Auth.js
+++ Server/middleware/user.Auth.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
- return  res.json({ cookies: req.cookies });
+ return  res.json({ cookies: req.headers.cookie });
   if (!req.cookies.token) {
 
     return res.json({ success: false, message: "Not Authorized my friend " });
