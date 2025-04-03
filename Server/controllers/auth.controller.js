f61eab1c13b5b44ca1234a9e6f77aa80ad3eda03
Ishan Saini <ishansaini0105@gmail.com>
2025-04-03 14:57:42 +0530

    Update auth.controller.js

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -87,7 +87,7 @@ export const login = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure,
       maxAge: 1 * 24 * 60 * 60 * 1000,
       sameSite: "none", 
     });
