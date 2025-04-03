6a49d3fecaca60b9dafec899ac17870b3f4b473c
Ishan Saini <ishansaini0105@gmail.com>
2025-04-03 17:16:37 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -12,10 +12,10 @@ DBconnect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser()); // for reading cookies, for sending we do not need it
-const allowedorigin = 'https://authzone-client.onrender.com'; // Deployed frontend URL
+
 
 app.use(cors({
-  origin: allowedorigin, // Allow requests from your frontend
+  origin: 'https://authzone-client.onrender.com', // Allow requests from your frontend
   credentials: true, // Allow cookies to be sent
 })); // by this we can send cookies
 
